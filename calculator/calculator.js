0818d47d016841594580316d2f71e1d580e5e19c
eempc <evc484@gmail.com>
2019-02-27 21:16:31 +0000

    font and some polish

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -8,26 +8,26 @@ const buttonsOp = document.querySelectorAll(".op");
 
 // All buttons get the press effect
 buttons.forEach(button => {
-    button.addEventListener("mouseup", function (e) {
-        e.target.style.boxShadow = "2px 2px";
-    });
-    button.addEventListener("mousedown", function (e) {
-        e.target.style.boxShadow = "-2px -2px";
-    });
+  button.addEventListener("mouseup", function(e) {
+    e.target.style.boxShadow = "2px 2px";
+  });
+  button.addEventListener("mousedown", function(e) {
+    e.target.style.boxShadow = "-2px -2px";
+  });
 });
 
 // Number buttons access this function
 buttonsNum.forEach(button => {
-    button.addEventListener("click", function (e) {
-        screenAppendNumber(e.target.textContent);
-    });
+  button.addEventListener("click", function(e) {
+    screenAppendNumber(e.target.textContent);
+  });
 });
 
 // Operator buttons access this function
 buttonsOp.forEach(button => {
-    button.addEventListener("click", function (e) {
-        screenAppendOperator(e.target.textContent);
-    });
+  button.addEventListener("click", function(e) {
+    screenAppendOperator(e.target.textContent);
+  });
 });
 
 // Empty array will have numbers and operators pushed to it
@@ -38,150 +38,148 @@ var wasTheLastButtonPressedEquals = false; // Detect if the calculator just calc
 
 // Function for numbers buttons
 function screenAppendNumber(x) {
-    if (wasTheLastButtonPressedEquals) {
-        clearAll();
-        wasTheLastButtonPressedEquals = false;
-        
-    }
-    screen1.textContent += x;
+  if (wasTheLastButtonPressedEquals) {
+    clearAll();
+    wasTheLastButtonPressedEquals = false;
+  }
+  screen1.textContent += x;
 }
 
 // Function for the operator buttons (is more complicated when trying to mimic an old school calculator)
 function screenAppendOperator(op) {
-    var str = screen1.textContent;
+  var str = screen1.textContent;
 
-    if (wasTheLastButtonPressedEquals) {
-        wasTheLastButtonPressedEquals = false
-        myArray = [];
-        myArray.push(str);
-        myArray.push(op);
-        updateScreen0();
-        clearScreen1();
-    } else {
-        if (!(str === "" || str === ".")) {
-            myArray.push(str);
-            myArray.push(op);
-            updateScreen0();
-            clearScreen1();
-        } else if (str === "" && screen0.textContent != "") {
-            myArray.pop();
-            myArray.push(op);
-            updateScreen0();
-        }
+  if (wasTheLastButtonPressedEquals) {
+    wasTheLastButtonPressedEquals = false;
+    myArray = [];
+    myArray.push(str);
+    myArray.push(op);
+    updateScreen0();
+    clearScreen1();
+  } else {
+    if (!(str === "" || str === ".")) {
+      myArray.push(str);
+      myArray.push(op);
+      updateScreen0();
+      clearScreen1();
+    } else if (str === "" && screen0.textContent != "") {
+      myArray.pop();
+      myArray.push(op);
+      updateScreen0();
     }
+  }
 }
 
 function updateScreen0() {
-    var fullEquation = "";
-    for (var i = 0; i < myArray.length; i++) {
-        //Detect the negative number and surround with brackets
-        if (myArray[i].charAt(0) === "-" && myArray[i].length > 1)
-            fullEquation += myArray[i];
-        else 
-            fullEquation += myArray[i];
-    }   
-    screen0.textContent = fullEquation;
+  var fullEquation = "";
+  for (var i = 0; i < myArray.length; i++) {
+    //Detect the negative number and surround with brackets
+    if (myArray[i].charAt(0) === "-" && myArray[i].length > 1)
+      fullEquation += myArray[i];
+    else fullEquation += myArray[i];
+  }
+  screen0.textContent = fullEquation;
 }
 
 function clearScreen0() {
-    screen0.textContent = "";
+  screen0.textContent = "";
 }
 
 function clearScreen1() {
-    screen1.textContent = "";
+  screen1.textContent = "";
 }
 
 function screenAppendDecimal() {
-    if (wasTheLastButtonPressedEquals) {
-        clearAll();
-        wasTheLastButtonPressedEquals = false;
-    }
-    var str = screen1.textContent;
-    if (!str.includes(".")) screen1.textContent += ".";
+  if (wasTheLastButtonPressedEquals) {
+    clearAll();
+    wasTheLastButtonPressedEquals = false;
+  }
+  var str = screen1.textContent;
+  if (!str.includes(".")) screen1.textContent += ".";
 }
 
 function makeNegative() {
-    var str = screen1.textContent;
-    wasTheLastButtonPressedEquals = false
-    if (!str.includes("-"))
-        screen1.textContent = "-" + str;
+  var str = screen1.textContent;
+  wasTheLastButtonPressedEquals = false;
+  if (!str.includes("-")) screen1.textContent = "-" + str;
 }
 
-function backspace(){
-    if (wasTheLastButtonPressedEquals) {
-        clearAll();
-        wasTheLastButtonPressedEquals = false;
-    }
-    if (screen1.textContent) {
-        screen1.textContent = screen1.textContent.slice(0, -1);
-    }
+function backspace() {
+  if (wasTheLastButtonPressedEquals) {
+    clearAll();
+    wasTheLastButtonPressedEquals = false;
+  }
+  if (screen1.textContent) {
+    screen1.textContent = screen1.textContent.slice(0, -1);
+  }
 }
 
 function clearAll() {
-    clearScreen1();
-    myArray = [];
-    clearScreen0();
-    wasTheLastButtonPressedEquals = false;
+  clearScreen1();
+  myArray = [];
+  clearScreen0();
+  wasTheLastButtonPressedEquals = false;
 }
 
 function operate() {
-    if (wasTheLastButtonPressedEquals) {
-        // Maybe one day I will do the continuous multiplier
-        return;
-    }
+  if (wasTheLastButtonPressedEquals) {
+    // Maybe one day I will do the continuous multiplier
+    return;
+  } else {
     wasTheLastButtonPressedEquals = true;
     var str = screen1.textContent;
-    
+
     if (str === "." || str === "") {
-        //If last element in the array is an operator
-        if (operatorsArray.includes(myArray[myArray.length-1])) {
-            myArray.pop();            
-            updateScreen0();
-        }       
-    } else {
-        myArray.push(str);
-        clearScreen1();
+      //If last element in the array is an operator
+      if (operatorsArray.includes(myArray[myArray.length - 1])) {
+        myArray.pop();
         updateScreen0();
+      }
+    } else {
+      myArray.push(str);
+      clearScreen1();
+      updateScreen0();
     }
-    
+
     // Modify the new array to do the multiplication and divisions first
     var newArray = myArray;
 
     for (var i = 0; i < newArray.length; i++) {
-        if (newArray[i+1] === "*") {
-            newArray.splice(i, 3, parseFloat(newArray[i]) * parseFloat(newArray[i+2])); // Do the multiplication
-            i--; // Go backwards 1 because the array was shortened
-        } 
-        if (newArray[i+1] === "/") {
-            if (newArray[i+2] == 0) {
-                alert("Divide by 0. Idiot.");
-                clearAll();
-                return;
-            }
-            newArray.splice(i, 3, parseFloat(newArray[i]) / parseFloat(newArray[i+2])); // Do the division
-            i--;
+      if (newArray[i + 1] === "*") {
+        newArray.splice(i,3,
+          parseFloat(newArray[i]) * parseFloat(newArray[i + 2])
+        ); // Do the multiplication
+        i--; // Go backwards 1 because the array was shortened
+      }
+      if (newArray[i + 1] === "/") {
+        if (newArray[i + 2] == 0) {
+          alert("Divide by 0. Idiot.");
+          clearAll();
+          return;
         }
+        newArray.splice(i, 3,
+          parseFloat(newArray[i]) / parseFloat(newArray[i + 2])
+        ); // Do the division
+        i--;
+      }
     }
-    
+
     // The final leg, an array that is only add or subtract, should be easy right?
     for (var i = 0; i < newArray.length; i++) {
-        if (newArray[i+1] === "+") {
-            newArray.splice(i, 3, parseFloat(newArray[i]) + parseFloat(newArray[i+2]));
-            i--;
-        } 
-        if (newArray[i+1] === "-") {
-            newArray.splice(i, 3, parseFloat(newArray[i]) - parseFloat(newArray[i+2]));
-            i--; 
-        }
+      if (newArray[i + 1] === "+") {
+        newArray.splice(i, 3,
+          parseFloat(newArray[i]) + parseFloat(newArray[i + 2])
+        );
+        i--;
+      }
+      if (newArray[i + 1] === "-") {
+        newArray.splice(i, 3,
+          parseFloat(newArray[i]) - parseFloat(newArray[i + 2])
+        );
+        i--;
+      }
     }
-    
-    screen1.textContent = (newArray[0] % 1 === 0) ? newArray[0] : newArray[0].toFixed(2);
-};
 
-var strMathsFunctions = {
-    '+': function (x, y) { return x + y },
-    '-': function (x, y) { return x - y },
-    '/': function (x, y) { return x / y },
-    '*': function (x, y) { return x * y },
-};
-
+    screen1.textContent = newArray[0] % 1 === 0 ? newArray[0] : newArray[0].toFixed(2);
+  }
+}
