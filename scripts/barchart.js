7d8c0af966386f7d9a02c012d4abcecf39ed17fc
eempc <evc484@gmail.com>
2019-03-30 21:37:52 +0000

    charts added

--- scripts/barchart.js
+++ scripts/barchart.js
@@ -7,6 +7,7 @@ function Comparator(a, b) {
 }
 
 // Load up my arrays and sort them because I can't be bothered to do it manually
+// Should become objects (tuple) in future
 var arrayCSharp = [
     [3, "Primitive types"],
     [3, "Windows Form controls"],
@@ -21,7 +22,8 @@ var arrayCSharp = [
     [0, "Events and exceptions"],
     [0, "Azure deployment"],
     [0, "Security"],
-    [0, "Unsafe methods"]
+    [0, "Unsafe methods"],
+    [3, "Recursive methods"]
 ];
 
 var arrayFrontEnd = [
@@ -32,6 +34,8 @@ var arrayFrontEnd = [
     [0, "Multi-browser support"],
     [0, "One JS Framework"],
     [1, "Forms"],
+    [3, "Event handlers"],
+    [0, "Bootstrap"]
 ];
 
 var arrayDataStructures = [
@@ -48,7 +52,7 @@ var arrayGeneral = [
     [2, "Git"],
     [1, "SLDC"],
     [0, "SOLID"],
-    [3, "Recursion"],
+
 ]
 
 function getArrayColumn(matrix, col) {
@@ -61,30 +65,32 @@ function getArrayColumn(matrix, col) {
 
 chooseChart(0);
 
+// Note to self: make objects
 function chooseChart(choice) {
     switch (choice) {
         case 0:
-            makeChart(arrayCSharp, "csharp-chart");
+            makeChart(arrayCSharp, "csharp-chart", "Programming Concepts (C#)");
             break;
         case 1:
-            makeChart(arrayFrontEnd, "frontend-chart");
+            makeChart(arrayFrontEnd, "frontend-chart", "Front End Knowledge");
             break;
         case 2:
-            makeChart(arrayDataStructures, "datastructures-chart");
+            makeChart(arrayDataStructures, "datastructures-chart", "Data Structures Knowledge");
             break;
         case 3:
-            makeChart(arrayGeneral, "general-chart");
+            makeChart(arrayGeneral, "general-chart", "General Software Development");
             break;
     }
 }
 
-function makeChart(my2DArray, chartID) {
+function makeChart(my2DArray, chartID, chartTitle) {
     my2DArray.sort(Comparator);
     var arrayAxisX = getArrayColumn(my2DArray, 0);
     var arrayAxisY = getArrayColumn(my2DArray, 1);
 
     var charts = document.getElementsByTagName("canvas");
 
+    // Make all charts disappear (alternatively could use z-index)
     for (var i = 0; i < charts.length; i++) {
         charts[i].style.display = "none";
     }
@@ -112,7 +118,7 @@ function makeChart(my2DArray, chartID) {
         options: {
             title: {
                 display: true,
-                text: "Test",
+                text: chartTitle,
                 fontSize: 20,
                 fontColor: "black"
             },
@@ -125,18 +131,31 @@ function makeChart(my2DArray, chartID) {
             },
             scales: {
                 yAxes: [{
+                    scaleLabel: {
+                        display: false,
+                        labelString: "Concept",
+                        fontColor: "black",
+                        fontSize: 14
+                    },
                     ticks: {
                         fontColor: "black",
                         fontSize: 10
                     }
                 }],
                 xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Competence level",
+                        fontColor: "black",
+                        fontSize: 14
+                    },
                     ticks: {
                         autoSkip: false,
                         maxRotation: 90,
                         fontColor: "black",
                         fontSize: 12,
                         stepSize: 1,
+
                         callback: function (value, index, values) {
                             //return '$' + value;
                             switch (true) {
