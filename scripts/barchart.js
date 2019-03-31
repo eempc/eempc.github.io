1e6906973cf5c9a9c04460cd96f87ef9244e1b63
eempc <evc484@gmail.com>
2019-03-31 10:07:01 +0100

    pseudo tab control for chart selection

--- scripts/barchart.js
+++ scripts/barchart.js
@@ -9,7 +9,7 @@ function Comparator(a, b) {
 // Load up my arrays and sort them because I can't be bothered to do it manually
 // Should become objects (tuple) in future
 var arrayCSharp = [
-    [3, "Primitive types"],
+    [3, "Primitive data types"],
     [3, "Windows Form controls"],
     [2, "Objects, classes, inheritance"],
     [2, "LINQ and SQL queries"],
@@ -19,7 +19,7 @@ var arrayCSharp = [
     [1, "Asychronous methods"],
     [1, "WPF XAML"],
     [1, "WPF data binding"],
-    [0, "Events and exceptions"],
+    [0, "Exceptions"],
     [0, "Azure deployment"],
     [0, "Security"],
     [0, "Unsafe methods"],
@@ -35,7 +35,8 @@ var arrayFrontEnd = [
     [0, "One JS Framework"],
     [1, "Forms"],
     [3, "Event handlers"],
-    [0, "Bootstrap"]
+    [0, "Bootstrap"],
+    [1, "Transforms"]
 ];
 
 var arrayDataStructures = [
@@ -49,10 +50,12 @@ var arrayDataStructures = [
 ];
 
 var arrayGeneral = [
-    [2, "Git"],
+    [3, "Windows environments"],
+    [2, "Ubuntu Linux environment"],
+    [2, "Git version control"],
     [1, "SLDC"],
     [0, "SOLID"],
-
+    [1, "TDD"]
 ]
 
 function getArrayColumn(matrix, col) {
@@ -63,6 +66,8 @@ function getArrayColumn(matrix, col) {
     return column;
 }
 
+
+let currentChartIndex = 0;
 chooseChart(0);
 
 // Note to self: make objects
@@ -70,32 +75,41 @@ function chooseChart(choice) {
     switch (choice) {
         case 0:
             makeChart(arrayCSharp, "csharp-chart", "Programming Concepts (C#)");
+            currentChartIndex = 0;
             break;
         case 1:
             makeChart(arrayFrontEnd, "frontend-chart", "Front End Knowledge");
+            currentChartIndex = 1;
             break;
         case 2:
             makeChart(arrayDataStructures, "datastructures-chart", "Data Structures Knowledge");
+            currentChartIndex = 2;
             break;
         case 3:
             makeChart(arrayGeneral, "general-chart", "General Software Development");
+            currentChartIndex = 3;
             break;
     }
+
+    var buttons = document.querySelectorAll("button");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = "darkgrey";
+    }
+    buttons[choice].style.backgroundColor = "blue";
 }
 
-function makeChart(my2DArray, chartID, chartTitle) {
-    my2DArray.sort(Comparator);
-    var arrayAxisX = getArrayColumn(my2DArray, 0);
-    var arrayAxisY = getArrayColumn(my2DArray, 1);
+function makeChart(dataMatrix2D, canvasID, chartTitle) {
+    dataMatrix2D.sort(Comparator);
+    var arrayAxisX = getArrayColumn(dataMatrix2D, 0);
+    var arrayAxisY = getArrayColumn(dataMatrix2D, 1);
 
-    var charts = document.getElementsByTagName("canvas");
-
-    // Make all charts disappear (alternatively could use z-index)
-    for (var i = 0; i < charts.length; i++) {
-        charts[i].style.display = "none";
+    var canvases = document.getElementsByTagName("canvas");
+    //Make all charts disappear (alternatively could use z-index)
+    for (var i = 0; i < canvases.length; i++) {
+        canvases[i].style.display = "none";
     }
 
-    var activeChart = document.getElementById(chartID);
+    var activeChart = document.getElementById(canvasID);
     activeChart.style.display = "block";
 
     var ctx = activeChart.getContext('2d');
@@ -120,7 +134,8 @@ function makeChart(my2DArray, chartID, chartTitle) {
                 display: true,
                 text: chartTitle,
                 fontSize: 20,
-                fontColor: "black"
+                fontColor: "black",
+                padding: 24
             },
             tooltips: {
                 enabled: false,
@@ -177,4 +192,3 @@ function makeChart(my2DArray, chartID, chartTitle) {
         }
     });
 }
-
